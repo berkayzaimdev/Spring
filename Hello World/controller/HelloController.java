774be86bcd7b6338372a9420eea324415f0d057c
Berkay Zaim <88087972+berkayzaimdev@users.noreply.github.com>
2023-01-26 23:59:14 +0300

    Add files via upload

---
+++ Hello World/controller/HelloController.java
@@ -0,0 +1,17 @@
+package My_Spring_Projects.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController
+{
+    //localhost:8080/hello
+
+    @GetMapping(path = "hello")
+    public String sayHello()
+    {
+        return "Hello World!";
+    }
+}
